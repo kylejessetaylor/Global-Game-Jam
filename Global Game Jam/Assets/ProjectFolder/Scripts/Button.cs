e944ca45a7d9048497e02365fe112b338358f09c
Kyle Taylor <kyle.jesse.taylor@gmail.com>
2018-01-27 01:29:16 +1100

    Added:  - Button.cs  - ButtonData.asset

---
+++ Global Game Jam/Assets/ProjectFolder/Scripts/Button.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Button", menuName = "ButtonData")]
+public class Button : ScriptableObject {
+
+	public bool land;
+	public int population;
+	public bool playerLocation;
+
+}
