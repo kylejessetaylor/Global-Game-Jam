39c444718f51c1d6d3737af508924d7b2f6107fd
CallanDavies <callan.davies1@gmail.com>
2018-01-27 09:33:37 +1100

    Pushing up updates to the ButtonClick, public land bool and playerLocationBool.

---
+++ Global Game Jam/Assets/ProjectFolder/Scripts/ButtonClick.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClick : MonoBehaviour
+{
+    public ButtonData buttonStats;
+
+    public bool land;
+
+
+    public int m_nPopulation;
+    public bool playerLocation;
+
+    // Use this for initialization
+    void Awake()
+    {
+        m_nPopulation = Random.Range(buttonStats.m_nMinimumPopulation, buttonStats.m_nMaximumPopulation);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void OnMouseClick()
+    {
+        GetComponent<Button>().interactable = false;
+        // cue animation, directed somehow to transform.position of this button.
+
+        //static score value += GetComponent<ButtonData>().population;
+        if (GetComponent<ButtonData>().playerLocation)
+        {
+            // player has been found,
+            // end game sstuff.
+        }
+        else
+        {
+            // player has not been found
+            // text cue
+            // sonar(?) pointing in general direction of player || hot/cold display.
+        }
+    }
+}
--- Global Game Jam/Assets/ProjectFolder/Scripts/ButtonData.cs
+++ Global Game Jam/Assets/ProjectFolder/Scripts/ButtonData.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Button", menuName = "ButtonData")]
-public class Button : ScriptableObject {
+public class ButtonData : ScriptableObject
+{
 
 	public bool land;
 	public int population;
 	public bool playerLocation;
 
+    public int m_nMinimumPopulation;
+    public int m_nMaximumPopulation;
 }
